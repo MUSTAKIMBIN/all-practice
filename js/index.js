b89168f02dc3d908111eed00287c35bf3bcd0ef2
MUSTAKIMBIN <mustakimbin24@gmail.com>
2025-07-29 23:38:41 +0600

    commit

--- js/index.js
+++ js/index.js
@@ -2,3 +2,15 @@ let age = 14;
 if (age < 14) {
   console.log("you are a child");
 }
+let myName = "Mustakim";
+// JavaScript has 8 types of data types
+/*1. string
+2.number
+3.boolean
+4.bigint
+5.undefine
+6.null
+7.symbol
+8.object
+
+*/
