0b2f3379688a8c46c1312a49c1a0730b84b40581
MUSTAKIMBIN <mustakimbin24@gmail.com>
2025-08-02 05:57:16 +0600

    practice while loop

---
+++ js/while.js
@@ -0,0 +1,32 @@
+const commitment = `I will invest at least 6 hrs every single day for next 60 days!`;
+let i = 1;
+// while (i <= 60) {
+//   console.log(commitment, i);
+//   i++;
+// }
+
+let num = 61;
+let sum = 0;
+while (num <= 100) {
+  if (num % 2 == 0) {
+    console.log(num);
+    sum += num;
+  }
+
+  num++;
+}
+console.log(sum);
+
+let score = 1;
+while (score <= 10) {
+  console.log(score * 5);
+  score++;
+}
+
+let time = 21;
+while (time >= 15) {
+  console.log(`` + time);
+
+  time--;
+}
+console.log(parseFloat(time));
