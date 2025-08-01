14c08e6c8b09b28a716e258a99a54376361967bd
MUSTAKIMBIN <mustakimbin24@gmail.com>
2025-08-02 04:46:07 +0600

    created conditional.js and solve one problem

---
+++ js/conditional.js
@@ -0,0 +1,11 @@
+/***
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+const burger = 500;
+if (burger > 500) {
+  console.log(`congratulation you got a free Coke`);
+} else {
+  console.log(`Your coke price: 30tk`);
+}
--- js/problem.js
+++ js/problem.js
@@ -4,7 +4,21 @@ if (price <= 6700) {
   //10% discount
   let discountPrice = (price * 10) / 100;
   let payAmount = price - discountPrice;
-  console.log(`you got 10% discount.You will pay  ${payAmount}`);
+  //   console.log(`you got 10% discount.You will pay  ${payAmount}`);
 } else {
   console.log(`you have to pay ${price}`);
+}
+// ternary operator
+let age = 12;
+age >= 18 ? console.log(`vot dite ja`) : console.log(`tor boyos hoi nai`);
+
+let money = 450;
+let isLeader = false;
+money = isLeader === true ? (money = 0) : (money += 100);
+console.log(money);
+
+// for of loop
+let forutes = ["am ", "jam", "kola", "mula"];
+for (const frout of forutes) {
+  console.log(frout);
 }
