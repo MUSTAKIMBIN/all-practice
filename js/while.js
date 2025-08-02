2cc1eff7ba552d57434563d645f07c3dc9cedfac
MUSTAKIMBIN <mustakimbin24@gmail.com>
2025-08-02 06:59:30 +0600

    created for.js file

--- js/while.js
+++ js/while.js
@@ -30,3 +30,12 @@ while (time >= 15) {
   time--;
 }
 console.log(parseFloat(time));
+
+const gess = 1n + 2n;
+console.log(gess);
+const x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const a = [x, x, x, x, x];
+for (let i = 0, j = 9; i <= j; i++, j--) {
+  //                              ^
+  console.log(`a[${i}][${j}]= ${a[i][j]}`);
+}
