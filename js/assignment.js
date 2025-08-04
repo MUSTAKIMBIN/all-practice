0a2f993837b2a0567ea7fe0ec489bc8b6ce5a035
MUSTAKIMBIN <mustakimbin24@gmail.com>
2025-08-04 15:33:42 +0600

    complited fith problem

--- js/assignment.js
+++ js/assignment.js
@@ -38,3 +38,9 @@ if (
 } else {
   console.log("Delete");
 }
+
+/** Problem 05 - ( PH Email Generator )  */
+var student = { name: "jhankar", roll: 1014, department: "cse" };
+//write your code here
+var studentMail = `${student.name}${student.roll}.${student.department}@ph.ac.bd`;
+console.log(studentMail);
