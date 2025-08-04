328159e8f10359206b450e7e995787a5d2c86687
MUSTAKIMBIN <mustakimbin24@gmail.com>
2025-08-04 15:25:05 +0600

    complited forth problem

--- js/assignment.js
+++ js/assignment.js
@@ -25,3 +25,16 @@ for (let i = 1; i <= lastDay; i++) {
     console.log(`${i} - rest`);
   }
 }
+
+/** Problem 04 - (Delete / Store) */
+var fileName = "pdfData.jpg";
+//write your code here
+if (
+  fileName.endsWith(".pdf") ||
+  fileName.endsWith(".docx") ||
+  fileName.startsWith("#")
+) {
+  console.log("Store");
+} else {
+  console.log("Delete");
+}
