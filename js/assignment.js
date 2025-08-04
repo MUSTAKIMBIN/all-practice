946f259f698b8f9cf6f76fbb79e2f9ae13f30d93
MUSTAKIMBIN <mustakimbin24@gmail.com>
2025-08-04 14:48:21 +0600

    complited second commit

--- js/assignment.js
+++ js/assignment.js
@@ -1,5 +1,16 @@
 /** Problem -01 ( Divide the Asset ) */
-var area = 100;
+var area = 800;
 //write your code here
 var divideArea = area / 2;
 console.log(divideArea);
+
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 10000;
+//write your code here
+if (money >= 25000) {
+  console.log("Laptop");
+} else if (money >= 10000) {
+  console.log("Cycle");
+} else {
+  console.log("Chocolate");
+}
