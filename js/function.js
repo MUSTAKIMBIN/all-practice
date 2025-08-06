7adf30ceaedb391e338c513505fae6558d0be0ee
MUSTAKIMBIN <mustakimbin24@gmail.com>
2025-08-06 15:03:43 +0600

    second function problem solved

--- js/function.js
+++ js/function.js
@@ -1,4 +1,11 @@
 function firstFuction() {
   console.log("I want a developer job");
 }
-firstFuction();
+// firstFuction();
+
+//Write a JavaScript function that reverses a number.
+function reverseNumber(num) {
+  num += "";
+  return num.split("").reverse().join("");
+}
+console.log(reverseNumber(123456));
