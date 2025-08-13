e5912223c7f5436d09f2d41553b327202f3fe478
MUSTAKIMBIN <mustakimbin24@gmail.com>
2025-08-13 15:15:27 +0600

    created practice.js

--- js/problem_part01.js
+++ js/problem_part01.js
@@ -57,4 +57,7 @@ function findAveragePhonePrice(phones) {
   return totalPhonPrice / phonLength;
 }
 const averageOfPhons = findAveragePhonePrice(phones);
-console.log(averageOfPhons);
+// console.log(averageOfPhons);
+
+// Task -5: (Hard)
+// For each employee their current salary is calculated by multiplying yearly increment with experience then adding the result to the starting salary. Now calculate is the total salary has to be provided by the company in a month.
